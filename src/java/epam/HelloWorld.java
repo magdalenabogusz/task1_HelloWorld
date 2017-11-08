14768e47cf27b80e52f59ceda3c1aeb632797361
Magdalena_Bogusz <magdalena_bogusz@epam.com>
2017-11-08 17:15:16 +0100

    task1: work in progress

---
+++ src/java/epam/HelloWorld.java
@@ -0,0 +1,23 @@
+package epam;
+
+/**
+ * Created by Magdalena_Bogusz on 11/8/2017.
+ */
+public class HelloWorld {
+
+    static int i = 0;
+    short j;
+    char c;
+    static int[][] arr = new int[3][3];
+    Integer k = new Integer(1);
+
+    public static void main(String[] args) {
+
+        Printer p = new Printer();
+        p.printString("khj");
+
+    }
+
+
+
+}
---
+++ src/java/epam/Printer.java
@@ -0,0 +1,17 @@
+package epam;
+
+/**
+ * Created by Magdalena_Bogusz on 11/8/2017.
+ */
+public class Printer {
+
+    String s;
+
+    public Printer() {
+        System.out.println("constructor");
+    }
+
+    public void printString(String s) {
+        System.out.println(s);
+    }
+}
